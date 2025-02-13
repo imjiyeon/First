092eaa54ee054be62b6f68c3ca9e326e56af5c35
shim tae hoon <sth2721@gmail.com>
2025-02-13 10:37:55 +0900

    새로운 프로젝트 생성

---
+++ src/hello/HelloJava.java
@@ -0,0 +1,22 @@
+package hello;
+
+/*
+ * 주석 여러줄
+ * 작성자: 임지연
+ * 작성일: 02/12 
+ *  */
+public class HelloJava {
+
+	// 주석이란? 프로그램의 설명
+	// 메인함수: 프로그램의 시작점
+	public static void main(String[] args) {
+		
+		// 문장을 출력하는 코드
+		// 문장을 완성할때는 세미콜론으로 마무리
+		// 문법에러가 발생이되면 빨간색으로 표시됨
+		System.out.println("imjiyeon, hi~");
+		
+		// ctrl + s
+	}
+
+}
